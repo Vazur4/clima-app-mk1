d6eefe78b39bca07728db5505b460d2e24facdc5
v4zur4 <acost4uri3l092@hotmail.com>
2025-10-15 17:28:13 -0600

    backeend

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,27 @@
-from flask import Flask, request, jsonify 
+from flask import Flask, request, jsonify
 import requests
+from flask_cors import CORS  # 🔹 Importante para permitir conexión con React
 
 app = Flask(__name__)
-API_KEY = "TuApiKeu"
+CORS(app)  # 🔹 Esto permite que React (frontend) se conecte al backend
+
+API_KEY = "b1df3a57b8704392c1f8c58446dfcfa5"  # Reemplaza con tu API Key real
+
 @app.route("/clima")
 def get_clima():
     ciudad = request.args.get("city")
-    url = f"https://api.openweathermap.org/data/2.5/weather?q={ciudad}&units=metric&appid={API_KEY}"
-    response = request.get(url)
-    return jsonify(response.json())
+    if not ciudad:
+        return jsonify({"error": "Debe proporcionar una ciudad"}), 400
+
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={ciudad}&units=metric&appid={API_KEY}&lang=es"
+    
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        return jsonify({"error": "No se pudo obtener el clima"}), response.status_code
+
+    data = response.json()
+    return jsonify(data)
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)
